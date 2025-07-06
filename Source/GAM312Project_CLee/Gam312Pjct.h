b7a436a9b03414dab069f453876ef90f28a1eeea
Carissa Lee <carissa.lee@snhu.edu>
2025-07-05 20:11:47 -0700

    Player Setup

---
+++ Source/GAM312Project_CLee/Gam312Pjct.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Gam312Pjct.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAM312PROJECT_CLEE_API AGam312Pjct : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
