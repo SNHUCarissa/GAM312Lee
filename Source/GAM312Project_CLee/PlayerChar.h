7f9c4bc2b2ee6512ee5f9afa87964a5e5710e352
Carissa Lee <carissa.lee@snhu.edu>
2025-07-10 20:14:15 -0700

    Added Player Stats

--- Source/GAM312Project_CLee/PlayerChar.h
+++ Source/GAM312Project_CLee/PlayerChar.h
@@ -46,4 +46,31 @@ public:
 	//Establishing the camera component to access the camera in code
 	UPROPERTY(VisibleAnywhere)
 		UCameraComponent* PlayerCamComp;
+
+	//Establishes a variable, allows user to edit the variable within the class, and access it in the Blueprints
+	//Default variable set to 100
+	//Category will allow us to see a section within the player character blueprint to access the new variables made
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Stats")
+		float Health = 100.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Stats")
+		float Hunger = 100.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Stats")
+		float Stamina = 100.0f;
+
+	//Custom events that are blueprint callable functions that will allow the user to set player stats and decrease them over
+	//time depending on each stat
+	UFUNCTION(BlueprintCallable)
+		void SetHealth(float amount);
+
+	UFUNCTION(BlueprintCallable)
+		void SetHunger(float amount);
+
+	UFUNCTION(BlueprintCallable)
+		void SetStamina(float amount);
+
+	UFUNCTION()
+		void DecreaseStats();
+
 };
